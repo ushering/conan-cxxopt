99f44d7118c3689f0ed2d6ecf1f5435a5dd74c28
Ruisheng Wang <rwang@wavecomp.com>
2019-07-18 15:51:58 -0700

    patch cxxopts for single dash format

--- conanfile.py
+++ conanfile.py
@@ -11,10 +11,12 @@ class CxxoptsConan(ConanFile):
     description = "Lightweight C++ command line option parser."
     homepage = "https://github.com/jarro2783/cxxopts"
     source_subfolder = "source_subfolder"
+    exports = "file.patch"
 
     def source(self):
         tools.get("https://github.com/jarro2783/cxxopts/archive/v{}.zip".format(self.version))
         shutil.move("cxxopts-{}/".format(self.version), self.source_subfolder)
+        tools.patch(base_path=self.source_subfolder, patch_file="file.patch")
 
     def package(self):
         cmake = CMake(self)
