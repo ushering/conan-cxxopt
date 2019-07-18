ba64d6c8ec53e40a4d5bd312bc7cfd7b963d8e12
Ruisheng Wang <rwang@wavecomp.com>
2019-07-18 15:23:05 -0700

    add recipe for cxxopts

---
+++ conanfile.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from conans import ConanFile, CMake, tools
+import shutil
+
+class CxxoptsConan(ConanFile):
+    name = "cxxopts"
+    version = "2.2.0"
+    license = "MIT"
+    author = "Ruisheng Wang (ruisheng.wang@outlook.com)"
+    url = "https://github.com/darcamo/conan-cxxopts"
+    description = "Lightweight C++ command line option parser."
+    homepage = "https://github.com/jarro2783/cxxopts"
+    source_subfolder = "source_subfolder"
+
+    def source(self):
+        tools.get("https://github.com/jarro2783/cxxopts/archive/v{}.zip".format(self.version))
+        shutil.move("cxxopts-{}/".format(self.version), self.source_subfolder)
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=self.source_subfolder)
+        cmake.install()
+
+    def package_id(self):
+        self.info.header_only()
