0c76ae526f681a0198969450bb98193d3c841a57
Ruisheng Wang <rwang@wavecomp.com>
2019-07-24 14:21:19 -0700

    exports sources

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 from conans import ConanFile, CMake, tools
 import shutil
+import os
 
 class CxxoptsConan(ConanFile):
     name = "cxxopts"
-    version = "2.2.0"
+    version = "2.2.0_src"
     license = "MIT"
     author = "Ruisheng Wang (ruisheng.wang@outlook.com)"
     url = "https://github.com/darcamo/conan-cxxopts"
@@ -12,8 +13,9 @@ class CxxoptsConan(ConanFile):
     homepage = "https://github.com/jarro2783/cxxopts"
     exports = "file.patch"
     _source_subfolder = "source_subfolder"
+    exports_sources = os.path.join(_source_subfolder, '*')
 
-    def source(self):
+    def _source(self):
         tools.get("https://github.com/jarro2783/cxxopts/archive/v{}.zip".format(self.version))
         shutil.move("cxxopts-{}/".format(self.version), self._source_subfolder)
         tools.patch(base_path=self._source_subfolder, patch_file="file.patch")
