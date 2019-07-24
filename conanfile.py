6d3aae52ac6a5f50738572681c58a412eb9dbef4
Ruisheng Wang <rwang@wavecomp.com>
2019-07-24 14:13:26 -0700

    update

--- conanfile.py
+++ conanfile.py
@@ -10,17 +10,17 @@ class CxxoptsConan(ConanFile):
     url = "https://github.com/darcamo/conan-cxxopts"
     description = "Lightweight C++ command line option parser."
     homepage = "https://github.com/jarro2783/cxxopts"
-    source_subfolder = "source_subfolder"
     exports = "file.patch"
+    _source_subfolder = "source_subfolder"
 
     def source(self):
         tools.get("https://github.com/jarro2783/cxxopts/archive/v{}.zip".format(self.version))
-        shutil.move("cxxopts-{}/".format(self.version), self.source_subfolder)
-        tools.patch(base_path=self.source_subfolder, patch_file="file.patch")
+        shutil.move("cxxopts-{}/".format(self.version), self._source_subfolder)
+        tools.patch(base_path=self._source_subfolder, patch_file="file.patch")
 
     def package(self):
         cmake = CMake(self)
-        cmake.configure(source_folder=self.source_subfolder)
+        cmake.configure(source_folder=self._source_subfolder)
         cmake.install()
 
     def package_id(self):
